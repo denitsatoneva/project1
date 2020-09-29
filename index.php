cdd75301fe02067b363e54ce5a3f8b947517ac76
radostin <es.skull@mail.bg>
2020-09-29 08:56:04 +0300

    if else and functions

--- index.php
+++ index.php
@@ -1,23 +1,69 @@
 <?php
-$username = "denitsatoneva";
-echo $username."<br>";
-$age = 13;
-echo $age."<br>";
-$istrue = true;
-echo $istrue."<br>";
-echo "My name is $username.<br>";
-$usernamearray = ["denitsa", "elmira"];
-echo $usernamearray[0];
-$usernameAssocArray = ["username" => "denitsatoneva"];
-echo $usernameAssocArray["username"];
-$usernameAssocArray = ["username" => "denitsatoneva", "age" => 22];
-echo $usernameAssocArray["age"];
-for($i=0; $i<2; $i++)
-{
-    echo $usernamearray[$i];  // izlizat ni dvete promenlivi ot masiva
-}
+// $username = "denitsatoneva";
+// echo $username."<br>";
+// $age = 13;
+// echo $age."<br>";
+// $istrue = true;
+// echo $istrue."<br>";
+// echo "My name is $username.<br>";
+// $usernamearray = ["denitsa", "elmira"];
+// echo $usernamearray[0];
+// $usernameAssocArray = ["username" => "denitsatoneva"];
+// echo $usernameAssocArray["username"];
+// $usernameAssocArray = ["username" => "denitsatoneva", "age" => 22];
+// echo $usernameAssocArray["age"];
+// for($i=0; $i<2; $i++)
+// {
+//     echo $usernamearray[$i];  // izlizat ni dvete promenlivi ot masiva
+// }
 
-foreach($usernameAssocArray as $key => $item)  //ciklenena sociativem masiv; suzdavame promenliva item
+// foreach($usernameAssocArray as $key => $item)  //ciklenena sociativem masiv; suzdavame promenliva item
+// {
+//     echo $key." ".$item."<br>";
+// }
+
+// $username = "oliviya";
+// if(1>2)
+// {
+//     echo "false";
+// }
+// elseif(2>1)
+// {
+//     echo "true";
+// }
+// else
+// {
+// echo "denitsa e qka";
+// }
+
+// switch($username)
+// {
+//     case $username = "oliviya":
+//         echo "She is Oliviya";
+//         break;
+//     case $username == "Mitko";
+//          echo "He is Mitko";
+//         break;
+//     default :
+//         echo "I don't know the name";
+//         break;
+// }
+
+function printName($username)
 {
-    echo $key." ".$item."<br>";
+    echo $username;
 }
+ printname("Elmira");
+
+//  function IsThisElmira($username)
+//  {
+//      if ($username == "Elmira")
+//     {
+//         echo "This is Elmira";
+//     }
+//     else
+//     {   
+//         echo "This is not Elmira";
+//     }
+//  }
+//  IsThisElmira("Elmira");
